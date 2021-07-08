1f78b8c317ce0c5fb2db13b2c6aea7dd87c52f03
Jaden Pleasants <jadenpleasants@fastmail.com>
2021-07-08 17:15:13 -0500

    Added way to get video url

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use youtube_dl::{YoutubeDl, YoutubeDlOutput};
+
 fn main() {
-    println!("Hello, world!");
+    let swamp = match YoutubeDl::new("https://www.youtube.com/watch?v=psFzJv8g6jc")
+        .socket_timeout("15")
+        .run()
+    {
+        Ok(a) => a,
+        _ => unreachable!(),
+    };
+    let video = match swamp {
+        YoutubeDlOutput::Playlist(_) => unreachable!(),
+        YoutubeDlOutput::SingleVideo(v) => v,
+    };
+    dbg!(&video.formats.unwrap()[0].url.as_ref().unwrap());
 }
